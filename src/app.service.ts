57ed82c79c7f7af896a87f0511702c48567e0205
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-27 12:20:47 +0700

    feat(app): add hello response for root route

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object {
+    return {
+      developer: 'Nguyen Minh Tuan - github.com/tnowad',
+      repository: 'https://github.com/tnowad/nest-comic',
+      description: 'This is NestComic API - Backend of NextComic',
+      version: '1.0',
+    };
   }
 }
