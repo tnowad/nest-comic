45b3fd8cc578ca7f01e82c9e1df35577a483447d
Nguyen Minh Tuan <tnowad@gmail.com>
2023-09-30 18:56:30 +0700

    feat(user): add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { Image } from 'src/images/entities/image.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column(() => Image)
+  avatar: Image;
+
+  @Column()
+  birthday: Date;
+
+  @Column()
+  bio: string;
+}
