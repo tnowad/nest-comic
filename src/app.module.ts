9347204caec99f0b8b67f1b34d76c9a76782da06
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-08 12:20:35 +0700

    feat: add config options

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import configOptions from './config/options.config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(configOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/options.config.ts
@@ -0,0 +1,5 @@
+import { ConfigModuleOptions } from '@nestjs/config';
+
+const configOptions: ConfigModuleOptions = {};
+
+export default configOptions;
