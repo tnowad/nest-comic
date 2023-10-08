a96c5f3fb93652648e98aaf3876fd8aabafb9b82
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-08 12:27:58 +0700

    feat: config `ConfigModuleOptions`

--- src/config/options.config.ts
+++ src/config/options.config.ts
@@ -1,5 +1,10 @@
 import { ConfigModuleOptions } from '@nestjs/config';
+import appConfig from './app.config';
 
-const configOptions: ConfigModuleOptions = {};
+const configOptions: ConfigModuleOptions = {
+  isGlobal: true,
+  load: [appConfig],
+  envFilePath: '.env',
+};
 
 export default configOptions;
