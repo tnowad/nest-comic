c9fce60e883785739c9551d689bf06eaf455a9a2
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-09 11:24:01 +0700

    feat: add setup typeorm connect postgres

---
+++ src/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Configurations, WasValidated } from 'src/config/configurations';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (
+        configService: ConfigService<Configurations, WasValidated>,
+      ) => {
+        return {
+          type: 'postgres',
+          url: configService.get('postgres.url', { infer: true }),
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
