7f1eb608fb65460cd1887dd2a080f8e72ef2e7f7
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-09 11:23:44 +0700

    feat: add postgres config to module

--- src/config/configurations.ts
+++ src/config/configurations.ts
@@ -1,7 +1,9 @@
+import { PostgresConfig } from 'src/database/config/postgres.config';
 import { AppConfig } from './app.config';
 
 export type Configurations = {
   app: AppConfig;
+  postgres: PostgresConfig;
 };
 
 export type WasValidated = true;
--- src/config/options.config.ts
+++ src/config/options.config.ts
@@ -1,9 +1,10 @@
 import { ConfigModuleOptions } from '@nestjs/config';
 import appConfig from './app.config';
+import postgresConfig from 'src/database/config/postgres.config';
 
 const configOptions: ConfigModuleOptions = {
   isGlobal: true,
-  load: [appConfig],
+  load: [appConfig, postgresConfig],
   envFilePath: '.env',
 };
 
