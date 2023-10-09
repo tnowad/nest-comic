775ff3a3ac6db53a6b9163a9b109d49eea2d9e8c
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-09 11:24:17 +0700

    feat: import database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import configOptions from './config/options.config';
 
 @Module({
-  imports: [ConfigModule.forRoot(configOptions)],
+  imports: [ConfigModule.forRoot(configOptions), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
