200cd2c2268f275d96556a45e23a98eeee537a83
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-16 00:19:43 +0700

    feat: add seed language data

--- src/seeds/seeds.module.ts
+++ src/seeds/seeds.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { SeedsService } from './seeds.service';
 
 @Module({
-  providers: [SeedsService]
+  providers: [SeedsService],
 })
 export class SeedsModule {}
--- src/seeds/seeds.service.ts
+++ src/seeds/seeds.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Language } from 'src/languages/entities/language.entity';
+import { EntityManager } from 'typeorm';
+import { languages } from './lanuages.data';
 
 @Injectable()
 export class SeedsService {
+  constructor(private readonly entityManager: EntityManager) {}
+
   async perform() {
-    console.log('SeedsService.perform()');
+    await this.entityManager.transaction(async (manager) => {
+      await this.createDefaultLanguage(manager);
+    });
+  }
+
+  async createDefaultLanguage(entityManager: EntityManager) {
+    entityManager.clear(Language);
+    await entityManager.save(Language, languages);
   }
 }
