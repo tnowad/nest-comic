8810575a876d6ddd08b88cc483a332b7ec1b999f
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-16 00:06:50 +0700

    chore: remove config logger in seeds script

--- scripts/seeds.ts
+++ scripts/seeds.ts
@@ -3,9 +3,7 @@ import { AppModule } from 'src/app.module';
 
 import { SeedsService } from 'src/seeds/seeds.service';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn'],
-  });
+  const app = await NestFactory.create(AppModule);
 
   const seedsService = app.get(SeedsService);
 
