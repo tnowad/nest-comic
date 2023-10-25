a8fd3af124676b92f342e8a033add5d06ab62114
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-25 15:14:00 +0700

    feat: add custom css and js for swagger

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,14 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   const configService = app.get(ConfigService<Configurations, WasValidated>);
 
