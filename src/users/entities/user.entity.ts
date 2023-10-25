66c783cbb2dc861633a1d411f7b8706c68de0cee
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-25 12:29:25 +0700

    feat: add user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,62 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum UserStatus {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { name: 'email', unique: true })
+  email: string;
+
+  @Column('text', { name: 'username', unique: true })
+  username: string;
+
+  @Column('text', { name: 'password' })
+  password: string;
+
+  @Column('text', { name: 'phone_number' })
+  phoneNumber: string;
+
+  @Column('text', { name: 'first_name' })
+  firstName: string;
+
+  @Column('text', { name: 'middle_name' })
+  middleName: string;
+
+  @Column('text', { name: 'last_name' })
+  lastName: string;
+
+  @Column('text', { name: 'full_name' })
+  fullName: string;
+
+  @Column('text', { name: 'nickname' })
+  nickname: string;
+
+  @Column('enum', {
+    name: 'status',
+    enum: UserStatus,
+    default: UserStatus.ACTIVE,
+  })
+  status: UserStatus;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at', type: 'timestamp' })
+  deletedAt: Date;
+}
