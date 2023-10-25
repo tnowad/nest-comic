91e7eb428f547f350f1f3c5393821967b6fd4e84
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-25 15:04:08 +0700

    feat: add api docs with swagger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import type { Configurations, WasValidated } from './config/configurations';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest Comic API')
+    .setDescription('The Comic API build with NestJS')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const configService = app.get(ConfigService<Configurations, WasValidated>);
 
