957056089657a1a7ae1c16c9c1401c8b2671a740
Nguyen Minh Tuan <tnowad@gmail.com>
2023-11-13 18:30:35 +0700

    feat: make `EmailSignUpDto` and `CreateUserDto` consistent

--- src/auth/dto/email-sign-up.dto.ts
+++ src/auth/dto/email-sign-up.dto.ts
@@ -5,14 +5,14 @@ import {
   IsString,
   Length,
   Matches,
+  MaxLength,
 } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { PASSWORD_REGEX, PHONE_NUMBER_REGEX } from '../../constants';
 
 export class EmailSignUpDto {
   @ApiProperty({ example: 'user@example.com' })
-  @IsString()
-  @IsEmail({}, { message: 'Email must be a valid email address' })
+  @IsEmail({}, { message: 'Must be a valid email address' })
   @IsNotEmpty({ message: 'Email is required' })
   email: string;
 
@@ -32,20 +32,32 @@ export class EmailSignUpDto {
     message: 'Phone number must be a valid phone number',
   })
   @IsOptional()
+  @MaxLength(20, {
+    message: 'Phone number cannot be longer than 20 characters',
+  })
   phoneNumber: string;
 
   @ApiProperty({ example: 'John' })
   @IsString()
   @IsNotEmpty({ message: 'First name cannot be empty' })
+  @MaxLength(50, { message: 'First name cannot be longer than 50 characters' })
   firstName: string;
 
   @ApiProperty({ example: 'Micheal' })
   @IsString()
   @IsOptional()
-  middleName?: string;
+  @MaxLength(50, { message: 'Middle name cannot be longer than 50 characters' })
+  middleName: string;
 
   @ApiProperty({ example: 'Smith' })
   @IsString()
   @IsNotEmpty({ message: 'Last name cannot be empty' })
+  @MaxLength(50, { message: 'Last name cannot be longer than 50 characters' })
   lastName: string;
+
+  @ApiProperty({ example: 'The Dark One' })
+  @IsString()
+  @IsOptional()
+  @MaxLength(50, { message: 'Nickname cannot be longer than 50 characters' })
+  nickname: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -4,54 +4,74 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
+  Length,
+  Matches,
   MaxLength,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { UserStatus } from '../entities/user.entity';
+import { PASSWORD_REGEX, PHONE_NUMBER_REGEX } from '../../constants';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'user@example.com' })
   @IsEmail({}, { message: 'Must be a valid email address' })
+  @IsNotEmpty({ message: 'Email is required' })
   email: string;
 
+  @ApiProperty({ example: 'Password123' })
   @IsString()
-  @IsNotEmpty({ message: 'Password cannot be empty' })
+  @IsNotEmpty({ message: 'Password is required' })
+  @Length(8, 50, { message: 'Password must be between 8 and 50 characters' })
+  @Matches(PASSWORD_REGEX, {
+    message:
+      'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number',
+  })
   password: string;
 
+  @ApiProperty({ example: '+1234567890' })
   @IsString()
+  @Matches(PHONE_NUMBER_REGEX, {
+    message: 'Phone number must be a valid phone number',
+  })
   @IsOptional()
   @MaxLength(20, {
     message: 'Phone number cannot be longer than 20 characters',
   })
   phoneNumber: string;
 
+  @ApiProperty({ example: 'John' })
   @IsString()
   @IsNotEmpty({ message: 'First name cannot be empty' })
   @MaxLength(50, { message: 'First name cannot be longer than 50 characters' })
   firstName: string;
 
+  @ApiProperty({ example: 'Micheal' })
   @IsString()
   @IsOptional()
   @MaxLength(50, { message: 'Middle name cannot be longer than 50 characters' })
   middleName: string;
 
+  @ApiProperty({ example: 'Smith' })
   @IsString()
   @IsNotEmpty({ message: 'Last name cannot be empty' })
   @MaxLength(50, { message: 'Last name cannot be longer than 50 characters' })
   lastName: string;
 
+  @ApiProperty({ example: 'John Micheal Smith' })
   @IsString()
   @IsOptional()
-  @MaxLength(100, { message: 'Full name cannot be longer than 100 characters' })
+  @MaxLength(150, { message: 'Full name cannot be longer than 150 characters' })
   fullName: string;
 
+  @ApiProperty({ example: 'The Dark One' })
   @IsString()
   @IsOptional()
   @MaxLength(50, { message: 'Nickname cannot be longer than 50 characters' })
   nickname: string;
 
+  @ApiProperty({ example: 'ACTIVE', enum: UserStatus })
   @IsEnum(UserStatus, {
-    message: `Status must be one of the following values: ${Object.values(
-      UserStatus,
-    ).join(', ')}`,
+    message: `Status must be one of the following values: ${Object.values(UserStatus).join(', ')}`,
   })
   status: UserStatus;
 }
