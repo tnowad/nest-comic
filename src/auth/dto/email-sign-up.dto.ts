3434629ddd25f9e79092899dc85998306f0c56ed
Nguyen Minh Tuan <tnowad@gmail.com>
2023-11-13 18:07:21 +0700

    feat: add email sign-up validation

--- src/auth/dto/email-sign-up.dto.ts
+++ src/auth/dto/email-sign-up.dto.ts
@@ -1,2 +1,51 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { PASSWORD_REGEX, PHONE_NUMBER_REGEX } from '../../constants';
+
 export class EmailSignUpDto {
+  @ApiProperty({ example: 'user@example.com' })
+  @IsString()
+  @IsEmail({}, { message: 'Email must be a valid email address' })
+  @IsNotEmpty({ message: 'Email is required' })
+  email: string;
+
+  @ApiProperty({ example: 'Password123' })
+  @IsString()
+  @IsNotEmpty({ message: 'Password is required' })
+  @Length(8, 50, { message: 'Password must be between 8 and 50 characters' })
+  @Matches(PASSWORD_REGEX, {
+    message:
+      'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number',
+  })
+  password: string;
+
+  @ApiProperty({ example: '+1234567890' })
+  @IsString()
+  @Matches(PHONE_NUMBER_REGEX, {
+    message: 'Phone number must be a valid phone number',
+  })
+  @IsOptional()
+  phoneNumber: string;
+
+  @ApiProperty({ example: 'John' })
+  @IsString()
+  @IsNotEmpty({ message: 'First name cannot be empty' })
+  firstName: string;
+
+  @ApiProperty({ example: 'Micheal' })
+  @IsString()
+  @IsOptional()
+  middleName?: string;
+
+  @ApiProperty({ example: 'Smith' })
+  @IsString()
+  @IsNotEmpty({ message: 'Last name cannot be empty' })
+  lastName: string;
 }
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+export const PHONE_NUMBER_REGEX =
+  /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
