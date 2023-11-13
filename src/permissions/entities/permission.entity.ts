50409f61b7456a74eafac64f90552d212a4ce5d7
Nguyen Minh Tuan <tnowad@gmail.com>
2023-11-13 21:02:54 +0700

    feat: add permission entity

---
+++ src/permissions/entities/permission.entity.ts
@@ -0,0 +1,21 @@
+import { MongoQuery } from '@casl/ability';
+import { AnyObject } from '@casl/ability/dist/types/types';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Permission<Entity = AnyObject> {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  action: string;
+
+  @Column()
+  subject: string;
+
+  @Column('json', { nullable: true })
+  conditions?: MongoQuery<Entity>;
+}
