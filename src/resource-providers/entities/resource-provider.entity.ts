b3e24329936f25101c96640becf185701a1976b1
Nguyen Minh Tuan <tnowad@gmail.com>
2023-11-19 11:01:36 +0700

    feat: add resource-provider entity

--- src/resource-providers/entities/resource-provider.entity.ts
+++ src/resource-providers/entities/resource-provider.entity.ts
@@ -1,1 +1,30 @@
-export class ResourceProvider {}
+import {
+  Column,
+  Entity,
+  Point,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'resource_providers',
+})
+export class ResourceProvider {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  name: string;
+
+  @Column('point', {
+    name: 'location',
+    nullable: true,
+  })
+  location: Point;
+
+  @Column('text')
+  description: string;
+
+  @Column('text')
+  url: string;
+
+}
