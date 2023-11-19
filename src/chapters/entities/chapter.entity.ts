c30c179a2aed52481b74fe37abb643b2a72f6337
Nguyen Minh Tuan <tnowad@gmail.com>
2023-11-19 11:02:46 +0700

    feat: add chapter entity

--- src/chapters/entities/chapter.entity.ts
+++ src/chapters/entities/chapter.entity.ts
@@ -1,1 +1,17 @@
-export class Chapter {}
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Chapter {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('decimal', {
+    name: 'order',
+  })
+  order: number;
+
+}
